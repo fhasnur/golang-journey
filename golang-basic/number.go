a2266b1b4a368daed42e306ca35c5ce3555a8a10
fhasnur <fandi.hasnur10@gmail.com>
2024-02-22 20:14:12 +0800

    number, boolean, string

---
+++ golang-basic/number.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Satu = ", 1)
+	fmt.Println("Datu = ", 2)
+	fmt.Println("Tiga koma lima = ", 3.5)
+}
---
+++ golang-basic/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Fandi")
+	fmt.Println("Fandi Meylwan")
+	fmt.Println("Fandi Meylwan Hasnur")
+
+	fmt.Println(len("Fandi"))
+	fmt.Println("Fandi Meylwan"[0])
+	fmt.Println("Fandi Meylwan Hasnur"[1])
+}
