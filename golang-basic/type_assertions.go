dcf8f75ed7b8fc52fcf590b262484e7a2252d9b8
fhasnur <fandi.hasnur10@gmail.com>
2024-03-02 22:39:24 +0800

    type assertions

---
+++ golang-basic/type_assertions.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func random() any {
+	// return "OK"
+	// return 100
+	return true
+}
+
+func main() {
+	var result any = random()
+	// var resultString string = result.(string)
+	// fmt.Println(resultString)
+	// var resultInt int = result.(int)
+	// fmt.Println(resultInt)
+
+	switch value := result.(type) {
+	case string:
+		fmt.Println("String", value)
+	case int:
+		fmt.Println("Int", value)
+	default:
+		fmt.Println("Unknowm", value)
+	}
+}
