198c64898024f9109f7934140497747a1d57a444
fhasnur <fandi.hasnur10@gmail.com>
2024-03-09 21:59:42 +0700

    access modifier

--- golang-basic/import.go
+++ golang-basic/import.go
@@ -8,4 +8,8 @@ import (
 func main() {
 	result := helper.SayHello("Fandi")
 	fmt.Println(result)
+
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.version) // tidak bisa diakses
+	// fmt.Println(helper.sayGoodBye("Fandi")) // tidak bisa diakses
 }
