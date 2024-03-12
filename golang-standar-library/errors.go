bd319ab4c30b2b577377ce2972a538ff0af5b67e
fhasnur <fandi.hasnur10@gmail.com>
2024-03-12 22:20:13 +0700

    errors

---
+++ golang-standar-library/errors.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ValidationError = errors.New("validation error")
+	NotFoundError   = errors.New("not found error")
+)
+
+func GetById(id string) error {
+	if id == "" {
+		return ValidationError
+	}
+
+	if id != "fandi" {
+		return NotFoundError
+	}
+
+	return nil
+}
+
+func main() {
+	err := GetById("fandi")
+	if err != nil {
+		if errors.Is(err, ValidationError) {
+			fmt.Println("validation error")
+		} else if errors.Is(err, NotFoundError) {
+			fmt.Println("not found error")
+		} else {
+			fmt.Println("unknown error")
+		}
+	}
+}
