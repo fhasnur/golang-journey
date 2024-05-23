03d6417c60776a06a34486efa05f364458b0d312
fhasnur <fandi.hasnur10@gmail.com>
2024-05-23 23:39:49 +0700

    create channel

---
+++ golang-goroutine/channel_test.go
@@ -0,0 +1,23 @@
+package golanggoroutine
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Fandi Meylwan Hasnur"
+		fmt.Println("Selesai mengirim data ke channel")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
