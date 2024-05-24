1c2d3765f5206121095fb241527e92ed8aa4b08c
fhasnur <fandi.hasnur10@gmail.com>
2024-05-24 21:16:54 +0700

    channel in & out

--- golang-goroutine/channel_test.go
+++ golang-goroutine/channel_test.go
@@ -38,3 +38,23 @@ func TestChannelAsParameter(t *testing.T) {
 
 	time.Sleep(5 * time.Second)
 }
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Fandi Meylwan Hasnur"
+}
+
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(5 * time.Second)
+}
