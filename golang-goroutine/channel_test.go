c96d60ebbb3de5d000cbd0d9d18d79db8f45bafd
fhasnur <fandi.hasnur10@gmail.com>
2024-05-24 21:09:25 +0700

    channel as parameter

--- golang-goroutine/channel_test.go
+++ golang-goroutine/channel_test.go
@@ -21,3 +21,20 @@ func TestCreateChannel(t *testing.T) {
 
 	time.Sleep(5 * time.Second)
 }
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Fandi Meylwan Hasnur"
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
