f82c214d269cfcd298967a2a9dd3fa5341e4aa68
fhasnur <fandi.hasnur10@gmail.com>
2024-05-27 21:39:15 +0700

    sync.Mutex

--- golang-goroutine/mutex_test.go
+++ golang-goroutine/mutex_test.go
@@ -2,17 +2,21 @@ package golanggoroutine
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 )
 
-func TestRaceCondition(t *testing.T) {
+func TestMutex(t *testing.T) {
 	x := 0
+	var mutex sync.Mutex
 
 	for i := 1; i <= 1000; i++ {
 		go func() {
 			for j := 1; j <= 100; j++ {
+				mutex.Lock()
 				x = x + 1
+				mutex.Unlock()
 			}
 		}()
 	}
