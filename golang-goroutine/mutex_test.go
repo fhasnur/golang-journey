2fd17c1024ee8ea456b0848bb0ca98abc68400f6
fhasnur <fandi.hasnur10@gmail.com>
2024-05-27 21:53:33 +0700

    sync.RWMutex

--- golang-goroutine/mutex_test.go
+++ golang-goroutine/mutex_test.go
@@ -24,3 +24,37 @@ func TestMutex(t *testing.T) {
 	time.Sleep(5 * time.Second)
 	fmt.Println("Counter = ", x)
 }
+
+type BankAccount struct {
+	RWMutext sync.RWMutex
+	Balance  int
+}
+
+func (account *BankAccount) AddBalance(amount int) {
+	account.RWMutext.Lock()
+	account.Balance = account.Balance + amount
+	account.RWMutext.Unlock()
+}
+
+func (account *BankAccount) GetBalance() int {
+	account.RWMutext.RLock()
+	balance := account.Balance
+	account.RWMutext.RUnlock()
+	return balance
+}
+
+func TestRWMutex(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalance())
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Total balance", account.GetBalance())
+}
