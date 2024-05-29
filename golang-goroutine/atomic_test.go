df51045bce88f5ed22e76cf7504f569ed27e8313
fhasnur <fandi.hasnur10@gmail.com>
2024-05-29 22:36:06 +0700

    atomic

---
+++ golang-goroutine/atomic_test.go
@@ -0,0 +1,26 @@
+package golanggoroutine
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+func TestAtomic(t *testing.T) {
+	var x int64 = 0
+	group := sync.WaitGroup{}
+
+	for i := 1; i <= 1000; i++ {
+		group.Add(1)
+		go func() {
+			for j := 1; j <= 100; j++ {
+				atomic.AddInt64(&x, 1)
+			}
+			group.Done()
+		}()
+	}
+
+	group.Wait()
+	fmt.Println("Counter = ", x)
+}
