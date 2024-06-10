c3d1c1768937777b91c95b241aa8329e5a4ac1a1
fhasnur <fandi.hasnur10@gmail.com>
2024-06-10 21:09:10 +0700

    query sql

--- golang-database/sql_test.go
+++ golang-database/sql_test.go
@@ -12,11 +12,35 @@ func TestExecSql(t *testing.T) {
 
 	ctx := context.Background()
 
-	script := "INSERT INTO customer(id, name) VALUES('fandi', 'Fandi')"
+	script := "INSERT INTO customer(id, name) VALUES('alam', 'Alam')"
 	_, err := db.ExecContext(ctx, script)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println("Success insert new customer")
+}
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id, name FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Id:", id)
+		fmt.Println("Name:", name)
+	}
 }
