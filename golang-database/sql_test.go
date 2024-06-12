39f04812ef00f9180325c036b56df33d25fdb4f5
fhasnur <fandi.hasnur10@gmail.com>
2024-06-12 23:26:01 +0700

    sql injection

--- golang-database/sql_test.go
+++ golang-database/sql_test.go
@@ -88,3 +88,33 @@ func TestQuerySqlComplex(t *testing.T) {
 		fmt.Println("Created At:", createdAt)
 	}
 }
+
+func TestSqlInjection(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "admin'; #"
+	password := "salah"
+
+	script := "SELECT username FROM user WHERE username = '" + username +
+		"' AND password = '" + password + "' LIMIT 1"
+	fmt.Println(script)
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		var username string
+		err = rows.Scan(&username)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Sukses Login", username)
+	} else {
+		fmt.Println("Gagal Login")
+	}
+}
