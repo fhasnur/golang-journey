83f038fff18eb82b5a942bab6ee4fbecd680b238
fhasnur <fandi.hasnur10@gmail.com>
2024-06-20 20:22:26 +0700

    request header

---
+++ golang-web/header_test.go
@@ -0,0 +1,28 @@
+package golangweb
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func RequestHeader(writer http.ResponseWriter, request *http.Request) {
+	contentType := request.Header.Get("content-type")
+	fmt.Fprint(writer, contentType)
+}
+
+func TestRequestHeader(t *testing.T) {
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/", nil)
+	request.Header.Add("Content-Type", "application/json")
+
+	recorder := httptest.NewRecorder()
+
+	RequestHeader(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
