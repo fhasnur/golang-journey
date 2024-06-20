8a3d9eb4cfa55054395f8fc6e6fbfc001b6aea27
fhasnur <fandi.hasnur10@gmail.com>
2024-06-20 20:28:30 +0700

    response header

--- golang-web/header_test.go
+++ golang-web/header_test.go
@@ -26,3 +26,25 @@ func TestRequestHeader(t *testing.T) {
 
 	fmt.Println(string(body))
 }
+
+func ResponseHeader(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Add("X-Powered-By", "Funday Space")
+	fmt.Fprint(writer, "OK")
+}
+
+func TestResponseHeader(t *testing.T) {
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/", nil)
+	request.Header.Add("Content-Type", "application/json")
+
+	recorder := httptest.NewRecorder()
+
+	ResponseHeader(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+
+	fmt.Println(response.Header.Get("x-powered-by"))
+
+}
