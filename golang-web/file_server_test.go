754f0ea79092b75c74aebfe15db99df182286df3
fhasnur <fandi.hasnur10@gmail.com>
2024-06-27 22:36:33 +0700

    file server

---
+++ golang-web/file_server_test.go
@@ -0,0 +1,24 @@
+package golangweb
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestFileServer(t *testing.T) {
+	directory := http.Dir("./resources")
+	fileServer := http.FileServer(directory)
+
+	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
