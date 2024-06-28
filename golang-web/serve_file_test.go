fdbb544609f57a13951ddabc1f3732dd453249e3
fhasnur <fandi.hasnur10@gmail.com>
2024-06-28 22:17:01 +0700

    serve file embed

--- golang-web/serve_file_test.go
+++ golang-web/serve_file_test.go
@@ -1,6 +1,8 @@
 package golangweb
 
 import (
+	_ "embed"
+	"fmt"
 	"net/http"
 	"testing"
 )
@@ -17,6 +19,32 @@ func TestServeFileServer(t *testing.T) {
 	server := http.Server{
 		Addr:    "localhost:8080",
 		Handler: http.HandlerFunc(ServeFile),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+//go:embed resources/ok.html
+var resourceOk string
+
+//go:embed resources/notfound.html
+var resourceNotFound string
+
+func ServeFileEmbed(writer http.ResponseWriter, request *http.Request) {
+	if request.URL.Query().Get("name") != "" {
+		fmt.Fprint(writer, resourceOk)
+	} else {
+		fmt.Fprint(writer, resourceNotFound)
+	}
+}
+
+func TestServeFileServerEmbed(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFileEmbed),
 	}
 
 	err := server.ListenAndServe()
