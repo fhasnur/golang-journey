1a5038c529ed4f08ba5f9791572ccd1e837a4f1c
fhasnur <fandi.hasnur10@gmail.com>
2024-06-28 22:11:49 +0700

    serve file

---
+++ golang-web/serve_file_test.go
@@ -0,0 +1,26 @@
+package golangweb
+
+import (
+	"net/http"
+	"testing"
+)
+
+func ServeFile(writer http.ResponseWriter, request *http.Request) {
+	if request.URL.Query().Get("name") != "" {
+		http.ServeFile(writer, request, "./resources/ok.html")
+	} else {
+		http.ServeFile(writer, request, "./resources/notfound.html")
+	}
+}
+
+func TestServeFileServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFile),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
