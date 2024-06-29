9d2cca80ebd6e4f85bbc82496c615ee62b85d0ec
fhasnur <fandi.hasnur10@gmail.com>
2024-06-29 23:04:34 +0700

    template text

---
+++ golang-web/template_test.go
@@ -0,0 +1,32 @@
+package golangweb
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func SimpleHTML(writer http.ResponseWriter, request *http.Request) {
+	templateText := `<html><body>{{.}}</body></html>`
+	// t, err := template.New("SIMPLE").Parse(templateText)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	t := template.Must(template.New("SIMPLE").Parse(templateText))
+
+	t.ExecuteTemplate(writer, "SIMPLE", "Hello HTML Template")
+}
+
+func TestSimpleHTML(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	SimpleHTML(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
