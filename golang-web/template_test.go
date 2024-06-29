3dec2d2a34605350eb70a2f1944cdb0e31d38699
fhasnur <fandi.hasnur10@gmail.com>
2024-06-29 23:25:21 +0700

    template embed

--- golang-web/template_test.go
+++ golang-web/template_test.go
@@ -1,6 +1,7 @@
 package golangweb
 
 import (
+	"embed"
 	"fmt"
 	"html/template"
 	"io"
@@ -58,6 +59,25 @@ func TestTemplateDirectory(t *testing.T) {
 	recorder := httptest.NewRecorder()
 
 	TemplateDirectory(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+//go:embed templates/*.gohtml
+var templates embed.FS
+
+func TemplateEmbed(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFS(templates, "templates/*.gohtml"))
+
+	t.ExecuteTemplate(writer, "simple.gohtml", "Hello HTML Template")
+}
+
+func TestTemplateEmbed(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateEmbed(recorder, request)
 
 	body, _ := io.ReadAll(recorder.Result().Body)
 	fmt.Println(string(body))
