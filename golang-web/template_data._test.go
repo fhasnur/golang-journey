ff1224cab1ca24e4b021c6ceede0b4dba1cdfbf7
fhasnur <fandi.hasnur10@gmail.com>
2024-07-02 21:19:07 +0700

    template data struct

--- golang-web/template_data._test.go
+++ golang-web/template_data._test.go
@@ -26,3 +26,34 @@ func TestTemplateDataMap(t *testing.T) {
 	body, _ := io.ReadAll(recorder.Result().Body)
 	fmt.Println(string(body))
 }
+
+type Page struct {
+	Title   string
+	Name    string
+	Address Address
+}
+
+type Address struct {
+	Street string
+}
+
+func TemplateDataStruct(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFS(templates, "templates/*.gohtml"))
+	t.ExecuteTemplate(writer, "name.gohtml", Page{
+		Title: "Template data Struct",
+		Name:  "Fandi",
+		Address: Address{
+			Street: "Jalan belum ada",
+		},
+	})
+}
+
+func TestTemplateDataStruct(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateDataStruct(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
