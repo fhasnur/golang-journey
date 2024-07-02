de304d3233ba58b85bf7fc37969a7f3d5ba6ce77
fhasnur <fandi.hasnur10@gmail.com>
2024-07-02 21:11:44 +0700

    template data map

---
+++ golang-web/template_data._test.go
@@ -0,0 +1,28 @@
+package golangweb
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TemplateDataMap(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFS(templates, "templates/*.gohtml"))
+	t.ExecuteTemplate(writer, "name.gohtml", map[string]interface{}{
+		"Title": "Template data Map",
+		"Name":  "Fandi",
+	})
+}
+
+func TestTemplateDataMap(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateDataMap(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
