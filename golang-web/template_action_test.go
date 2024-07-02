50ea5415af3db50ed5a178da8c3ac52a666b22e2
fhasnur <fandi.hasnur10@gmail.com>
2024-07-02 21:34:32 +0700

    template action if

---
+++ golang-web/template_action_test.go
@@ -0,0 +1,28 @@
+package golangweb
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TemplateActionIf(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/if.gohtml"))
+	t.ExecuteTemplate(writer, "if.gohtml", Page{
+		Title: "Template action if",
+		Name:  "Fandi",
+	})
+}
+
+func TestTemplateActionIf(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionIf(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
