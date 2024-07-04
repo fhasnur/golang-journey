03dc5277f0285bfec7b92ce6529acb45e6bbdc27
fhasnur <fandi.hasnur10@gmail.com>
2024-07-04 22:09:35 +0700

    template layout

---
+++ golang-web/template_layout_test.go
@@ -0,0 +1,32 @@
+package golangweb
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TemplateLayout(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles(
+		"./templates/header.gohtml",
+		"./templates/layout.gohtml",
+		"./templates/footer.gohtml",
+	))
+	t.ExecuteTemplate(writer, "layout.gohtml", map[string]interface{}{
+		"Title": "Template Layout",
+		"Name":  "Fandi",
+	})
+}
+
+func TestTemplateLayouth(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateLayout(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
