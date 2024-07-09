08ca3ac9abe13386a8ee28e3b50e86201927ce1b
fhasnur <fandi.hasnur10@gmail.com>
2024-07-09 16:51:54 +0700

    template function

---
+++ golang-web/template_function_test.go
@@ -0,0 +1,35 @@
+package golangweb
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"text/template"
+)
+
+type MyPage struct {
+	Name string
+}
+
+func (myPage MyPage) SayHello(name string) string {
+	return "Hello " + name + ", my name is " + myPage.Name
+}
+
+func TemplateFunction(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.New("FUNCTION").Parse(`{{.SayHello "Budi"}}`))
+	t.ExecuteTemplate(writer, "FUNCTION", MyPage{
+		Name: "Fandi",
+	})
+}
+
+func TestTemplateFunction(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateFunction(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
