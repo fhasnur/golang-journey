3be5040e4b905363725697808c0d7935dedec977
fhasnur <fandi.hasnur10@gmail.com>
2024-07-14 22:18:49 +0700

    upload file

---
+++ golang-web/upload_file_test.go
@@ -0,0 +1,47 @@
+package golangweb
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"testing"
+)
+
+func UploadForm(writer http.ResponseWriter, request *http.Request) {
+	myTemplates.ExecuteTemplate(writer, "upload.form.gohtml", nil)
+}
+
+func Upload(writer http.ResponseWriter, request *http.Request) {
+	// request.ParseMultipartForm(32 << 20)
+	file, fileHeader, err := request.FormFile("file")
+	if err != nil {
+		panic(err)
+	}
+	fileDestination, err := os.Create("./resources/" + fileHeader.Filename)
+	_, err = io.Copy(fileDestination, file)
+	if err != nil {
+		panic(err)
+	}
+	name := request.PostFormValue("name")
+	myTemplates.ExecuteTemplate(writer, "upload.success.gohtml", map[string]interface{}{
+		"Name": name,
+		"File": "/static/" + fileHeader.Filename,
+	})
+}
+
+func TestUploadForm(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", UploadForm)
+	mux.HandleFunc("/upload", Upload)
+	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./resources"))))
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
