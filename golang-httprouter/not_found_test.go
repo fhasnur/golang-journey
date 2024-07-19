e682f6133627ce7b5b8e3da14a650dd15d424a60
fhasnur <fandi.hasnur10@gmail.com>
2024-07-19 21:05:18 +0700

    not found handler

---
+++ golang-httprouter/not_found_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNotFound(t *testing.T) {
+	router := httprouter.New()
+
+	router.NotFound = http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Gak ketemu")
+	})
+
+	request := httptest.NewRequest("GET", "http://localhost:3000/404", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Gak ketemu", string(body))
+}
