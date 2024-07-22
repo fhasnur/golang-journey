e935d3d2d3ddba74647a8920eda3a046792e0d8b
fhasnur <fandi.hasnur10@gmail.com>
2024-07-22 23:50:33 +0700

    json object

---
+++ golang-json/json_object_test.go
@@ -0,0 +1,27 @@
+package golangjson
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+type Customer struct {
+	FirstName  string
+	MiddleName string
+	LastName   string
+	Age        int
+	Married    bool
+}
+
+func TestJSONObject(t *testing.T) {
+	customer := Customer{
+		FirstName:  "Fandi",
+		MiddleName: "Meylwan",
+		LastName:   "Hasnur",
+		Age:        25,
+		Married:    false,
+	}
+	bytes, _ := json.Marshal(customer)
+	fmt.Println(string(bytes))
+}
