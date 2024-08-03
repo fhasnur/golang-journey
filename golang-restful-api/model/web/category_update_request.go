5e29283ce3dd0217290ccfb59eb26aceaf1a9eb7
fhasnur <fandi.hasnur10@gmail.com>
2024-08-04 06:39:56 +0700

    modify to json tag, fix issues

--- golang-restful-api/model/web/category_update_request.go
+++ golang-restful-api/model/web/category_update_request.go
@@ -2,5 +2,5 @@ package web
 
 type CategoryUpdateRequest struct {
 	Id   int    `validate:"required"`
-	Name string `validate:"required,max=200,min=1"`
+	Name string `validate:"required,max=200,min=1" json:"name"`
 }
--- golang-restful-api/repository/category_repository_impl.go
+++ golang-restful-api/repository/category_repository_impl.go
@@ -16,7 +16,7 @@ func NewCategoryRepository() CategoryRepository {
 }
 
 func (repository *CategoryRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, category domain.Category) domain.Category {
-	SQL := "INSERT INTO customer(name) VALUES (?)"
+	SQL := "INSERT INTO category(name) VALUES (?)"
 	result, err := tx.ExecContext(ctx, SQL, category.Name)
 	helper.PanicIfError(err)
 
@@ -45,6 +45,7 @@ func (repository *CategoryRepositoryImpl) FindById(ctx context.Context, tx *sql.
 	SQL := "SELECT id, name FROM category WHERE id = ?"
 	rows, err := tx.QueryContext(ctx, SQL, categoryId)
 	helper.PanicIfError(err)
+	defer rows.Close()
 
 	category := domain.Category{}
 	if rows.Next() {
@@ -60,6 +61,7 @@ func (repository *CategoryRepositoryImpl) FindAll(ctx context.Context, tx *sql.T
 	SQL := "SELECT id, name FROM category"
 	rows, err := tx.QueryContext(ctx, SQL)
 	helper.PanicIfError(err)
+	defer rows.Close()
 
 	var categories []domain.Category
 	for rows.Next() {
