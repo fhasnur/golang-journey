7823ac8c49f9accbd728489f17df3514f8171b2b
fhasnur <fandi.hasnur10@gmail.com>
2024-08-19 10:55:24 +0700

    injector parameter

--- golang-dependency-injection/test/simple_service_test.go
+++ golang-dependency-injection/test/simple_service_test.go
@@ -1,13 +1,20 @@
 package test
 
 import (
-	"fmt"
 	"golang-restful-api/simple"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestSimpleService(t *testing.T) {
-	simpleService, err := simple.InitializedService()
-	fmt.Println(err)
-	fmt.Println(simpleService)
+func TestSimpleServiceError(t *testing.T) {
+	simpleService, err := simple.InitializedService(true)
+	assert.NotNil(t, err)
+	assert.Nil(t, simpleService)
+}
+
+func TestSimpleServiceSuccess(t *testing.T) {
+	simpleService, err := simple.InitializedService(false)
+	assert.Nil(t, err)
+	assert.NotNil(t, simpleService)
 }
