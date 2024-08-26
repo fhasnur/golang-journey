063e2ec41f12be9ade612a143f2bc5b6688cd52b
fhasnur <fandi.hasnur10@gmail.com>
2024-08-26 20:58:00 +0700

    binding values

--- golang-dependency-injection/simple/injector.go
+++ golang-dependency-injection/simple/injector.go
@@ -3,7 +3,12 @@
 
 package simple
 
-import "github.com/google/wire"
+import (
+	"io"
+	"os"
+
+	"github.com/google/wire"
+)
 
 func InitializedService(isError bool) (*SimpleService, error) {
 	wire.Build(
@@ -47,5 +52,18 @@ func InitializedHelloService() *HelloService {
 
 func InitializedFooBar() *FooBar {
 	wire.Build(NewFoo, NewBar, wire.Struct(new(FooBar), "*"))
+	return nil
+}
+
+var fooValue = &Foo{}
+var barValue = &Bar{}
+
+func InitializedFooBarUsingValue() *FooBar {
+	wire.Build(wire.Value(fooValue), wire.Value(barValue), wire.Struct(new(FooBar), "*"))
+	return nil
+}
+
+func InitializedReader() io.Reader {
+	wire.Build(wire.InterfaceValue(new(io.Reader), os.Stdin))
 	return nil
 }
