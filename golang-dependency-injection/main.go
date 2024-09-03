ec5851728160de6c35c56464e4bcf0edfdbc2a3c
fhasnur <fandi.hasnur10@gmail.com>
2024-09-03 22:10:19 +0700

    dependency injection in restful api

--- golang-dependency-injection/main.go
+++ golang-dependency-injection/main.go
@@ -1,33 +1,22 @@
 package main
 
 import (
-	"golang-restful-api/app"
-	"golang-restful-api/controller"
 	"golang-restful-api/helper"
 	"golang-restful-api/middleware"
-	"golang-restful-api/repository"
-	"golang-restful-api/service"
 	"net/http"
 
-	"github.com/go-playground/validator"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-
-	db := app.NewDB()
-	validate := validator.New()
-	categoryRepository := repository.NewCategoryRepository()
-	categoryService := service.NewCategoryService(categoryRepository, db, validate)
-	categoryController := controller.NewCategoryController(categoryService)
-
-	router := app.NewRouter(categoryController)
-
-	server := http.Server{
+func NewServer(authMiddleware *middleware.AuthMiddleware) *http.Server {
+	return &http.Server{
 		Addr:    "localhost:3000",
-		Handler: middleware.NewAuthMiddleware(router),
+		Handler: authMiddleware,
 	}
+}
 
+func main() {
+	server := InitializedServer()
 	err := server.ListenAndServe()
 	helper.PanicIfError(err)
 }
