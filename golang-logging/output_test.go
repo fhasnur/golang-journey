dc076addf563eec72b60e6f06dc4ef8ffcbd4757
fhasnur <fandi.hasnur10@gmail.com>
2024-09-05 21:47:43 +0700

    output

---
+++ golang-logging/output_test.go
@@ -0,0 +1,18 @@
+package golanglogging
+
+import (
+	"os"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+func TestOutput(t *testing.T) {
+	logger := logrus.New()
+	file, _ := os.OpenFile("application.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	logger.SetOutput(file)
+
+	logger.Info("Hello Logging")
+	logger.Warn("Hello Logging")
+	logger.Error("Hello Logging")
+}
