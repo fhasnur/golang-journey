d5688c2ca7bf6140149c9a5e57dd7a12144dc4cd
fhasnur <fandi.hasnur10@gmail.com>
2024-09-05 21:31:46 +0700

    level

---
+++ golang-logging/level_test.go
@@ -0,0 +1,28 @@
+package golanglogging
+
+import (
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+func TestLevel(t *testing.T) {
+	logger := logrus.New()
+
+	logger.Trace("This is Trace")
+	logger.Debug("This is Debug")
+	logger.Info("This is Info")
+	logger.Warn("This is Warn")
+	logger.Error("This is Error")
+}
+
+func TestLoggingLevel(t *testing.T) {
+	logger := logrus.New()
+	logger.SetLevel(logrus.TraceLevel)
+
+	logger.Trace("This is Trace")
+	logger.Debug("This is Debug")
+	logger.Info("This is Info")
+	logger.Warn("This is Warn")
+	logger.Error("This is Error")
+}
