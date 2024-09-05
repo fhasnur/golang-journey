60a780cb60de8c19933f38856653976d77f3f25d
fhasnur <fandi.hasnur10@gmail.com>
2024-09-05 21:18:26 +0700

    logger

---
+++ golang-logging/logger_test.go
@@ -0,0 +1,15 @@
+package golanglogging
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+func TestLogger(t *testing.T) {
+	logger := logrus.New()
+
+	logger.Println("Hello Logger")
+	fmt.Println("Hello Logger")
+}
