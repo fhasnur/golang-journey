5cebcbf90019425625b4f35ba889ff32b49749a8
fhasnur <fandi.hasnur10@gmail.com>
2024-09-06 21:05:37 +0700

    entry

---
+++ golang-logging/entry_test.go
@@ -0,0 +1,16 @@
+package golanglogging
+
+import (
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+func TestEntry(t *testing.T) {
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.JSONFormatter{})
+
+	entry := logrus.NewEntry(logger)
+	entry.WithField("username", "fhasnur").
+		Info("Hello Entry")
+}
