7fbcb68e9b1cb556336afc33a90893f13bd57e20
fhasnur <fandi.hasnur10@gmail.com>
2024-09-24 22:10:00 +0700

    multiple type parameter

---
+++ golang-generics/multiple_type_parameter_test.go
@@ -0,0 +1,16 @@
+package golanggenerics
+
+import (
+	"fmt"
+	"testing"
+)
+
+func MultipleParameter[T1 any, T2 any](param1 T1, param2 T2) {
+	fmt.Println(param1)
+	fmt.Println(param2)
+}
+
+func TestMultipleParameter(t *testing.T) {
+	MultipleParameter[string, int]("Fandi", 100)
+	MultipleParameter[int, string](100, "Fandi")
+}
