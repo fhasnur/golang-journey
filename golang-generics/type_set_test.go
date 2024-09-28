37b5d9e328c4d37629f23eea3c51335fdf8c341b
fhasnur <fandi.hasnur10@gmail.com>
2024-09-28 22:13:20 +0700

    type approximation

--- golang-generics/type_set_test.go
+++ golang-generics/type_set_test.go
@@ -6,8 +6,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type Age int
+
 type Number interface {
-	int | int8 | int16 | int32 | int64 |
+	~int | int8 | int16 | int32 | int64 |
 		float32 | float64
 }
 
@@ -23,4 +25,5 @@ func TestMin(t *testing.T) {
 	assert.Equal(t, 100, Min[int](100, 200))
 	assert.Equal(t, int64(100), Min[int64](int64(100), int64(200)))
 	assert.Equal(t, float64(100), Min[float64](float64(100), float64(200)))
+	assert.Equal(t, Age(100), Min[Age](Age(100), Age(200))) // Type approximation
 }
