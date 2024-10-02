138d459323739296da0b8950da5e02f13e1ffd65
fhasnur <fandi.hasnur10@gmail.com>
2024-10-02 08:30:06 +0700

    inline type constraint

---
+++ golang-generics/inline_test.go
@@ -0,0 +1,35 @@
+package golanggenerics
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func FindMin[T interface{ int | int64 | float64 }](first, second T) T {
+	if first < second {
+		return first
+	} else {
+		return second
+	}
+}
+
+func TestFindMin(t *testing.T) {
+	assert.Equal(t, 100, FindMin[int](100, 200))
+	assert.Equal(t, int64(100), FindMin[int64](int64(100), int64(200)))
+	assert.Equal(t, 100.0, FindMin(100.0, 200.0))
+}
+
+func GetFirst[T []E, E any](slice T) E {
+	first := slice[0]
+	return first
+}
+
+func TestGetFirst(t *testing.T) {
+	names := []string{
+		"Fandi", "Meylwan", "Hasnur",
+	}
+
+	first := GetFirst[[]string, string](names)
+	assert.Equal(t, "Fandi", first)
+}
