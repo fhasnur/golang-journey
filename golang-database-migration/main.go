b3b731760166a15c64508a25d44d723c87bc3499
fhasnur <fandi.hasnur10@gmail.com>
2024-10-09 00:08:46 +0700

    go database migration

---
+++ golang-database-migration/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"golang-restful-api/app"
+	"golang-restful-api/controller"
+	"golang-restful-api/helper"
+	"golang-restful-api/middleware"
+	"golang-restful-api/repository"
+	"golang-restful-api/service"
+	"net/http"
+
+	"github.com/go-playground/validator"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+
+	db := app.NewDB()
+	validate := validator.New()
+	categoryRepository := repository.NewCategoryRepository()
+	categoryService := service.NewCategoryService(categoryRepository, db, validate)
+	categoryController := controller.NewCategoryController(categoryService)
+
+	router := app.NewRouter(categoryController)
+
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: middleware.NewAuthMiddleware(router),
+	}
+
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
+}
