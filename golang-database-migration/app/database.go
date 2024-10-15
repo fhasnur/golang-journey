c3f2e77be350137e3ed96caef5fbfe5a56e5477c
fhasnur <fandi.hasnur10@gmail.com>
2024-10-15 18:24:32 +0700

    change database

--- golang-database-migration/app/database.go
+++ golang-database-migration/app/database.go
@@ -7,7 +7,7 @@ import (
 )
 
 func NewDB() *sql.DB {
-	db, err := sql.Open("mysql", "root:123456789@tcp(localhost:3306)/golang_restful_api")
+	db, err := sql.Open("mysql", "root:123456789@tcp(localhost:3306)/golang_database_migration")
 	helper.PanicIfError(err)
 
 	db.SetMaxIdleConns(5)
