5a1ddd58dcbbbf96aff1acfeedad79c1215b3bd9
fhasnur <fandi.hasnur10@gmail.com>
2024-10-30 22:05:28 +0700

    validation struct

---
+++ golang-validation/validation_test.go
@@ -0,0 +1,14 @@
+package golangvalidation
+
+import (
+	"testing"
+
+	"github.com/go-playground/validator/v10"
+)
+
+func TestValidation(t *testing.T) {
+	var validate *validator.Validate = validator.New()
+	if validate == nil {
+		t.Error("Validate is nil")
+	}
+}
