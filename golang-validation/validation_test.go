696f58f48ac5d3f88cbfab9b4046c2b22f96f0ef
fhasnur <fandi.hasnur10@gmail.com>
2024-10-31 20:38:51 +0700

    validation variable

--- golang-validation/validation_test.go
+++ golang-validation/validation_test.go
@@ -1,6 +1,7 @@
 package golangvalidation
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/go-playground/validator/v10"
@@ -10,5 +11,15 @@ func TestValidation(t *testing.T) {
 	var validate *validator.Validate = validator.New()
 	if validate == nil {
 		t.Error("Validate is nil")
+	}
+}
+
+func TestValidationVariable(t *testing.T) {
+	validate := validator.New()
+	user := "fandi"
+
+	err := validate.Var(user, "required")
+	if err != nil {
+		fmt.Println(err.Error())
 	}
 }
