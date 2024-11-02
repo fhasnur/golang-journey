874c69e2abf1cbd371798d0a7b3a8d900098998b
fhasnur <fandi.hasnur10@gmail.com>
2024-11-02 20:44:53 +0700

    basic collection validation

--- golang-validation/validation_test.go
+++ golang-validation/validation_test.go
@@ -143,6 +143,46 @@ func TestNestedStruct(t *testing.T) {
 	}
 }
 
+func TestBasicCollection(t *testing.T) {
+	type Address struct {
+		City    string `validate:"required"`
+		Country string `validate:"required"`
+	}
+
+	type User struct {
+		Id        string    `validate:"required"`
+		Name      string    `validate:"required"`
+		Addresses []Address `validate:"required,dive"`
+		Hobbies   []string  `validate:"required,dive,required,min=3"`
+	}
+
+	validate := validator.New()
+	request := User{
+		Id:   "1",
+		Name: "Fandi",
+		Addresses: []Address{
+			{
+				City:    "Baubau",
+				Country: "Indonesia",
+			},
+			{
+				City:    "Jakarta",
+				Country: "Indonesia",
+			},
+		},
+		Hobbies: []string{
+			"Parkour",
+			"Coding",
+			"Design",
+		},
+	}
+
+	err := validate.Struct(request)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
 func TestCollection(t *testing.T) {
 	type Address struct {
 		City    string `validate:"required"`
