7dbe2b566be6616f995490e1d5f966a671cc0fb2
fhasnur <fandi.hasnur10@gmail.com>
2024-11-07 21:01:54 +0700

    environment variable

--- golang-viper/viper_test.go
+++ golang-viper/viper_test.go
@@ -62,3 +62,22 @@ func TestENVFile(t *testing.T) {
 	assert.Equal(t, 3306, config.GetInt("DATABASE_PORT"))
 	assert.Equal(t, true, config.GetBool("DATABASE_SHOW_SQL"))
 }
+
+func TestENV(t *testing.T) {
+	config := viper.New()
+	config.SetConfigFile("config.env")
+	config.AddConfigPath(".")
+	config.AutomaticEnv()
+
+	// read config
+	err := config.ReadInConfig()
+	assert.Nil(t, err)
+
+	assert.Equal(t, "golang-viper", config.GetString("APP_NAME"))
+	assert.Equal(t, "Fandi Hasnur", config.GetString("APP_AUTHOR"))
+	assert.Equal(t, "localhost", config.GetString("DATABASE_HOST"))
+	assert.Equal(t, 3306, config.GetInt("DATABASE_PORT"))
+	assert.Equal(t, true, config.GetBool("DATABASE_SHOW_SQL"))
+
+	assert.Equal(t, "Hello", config.GetString("FROM_ENV"))
+}
