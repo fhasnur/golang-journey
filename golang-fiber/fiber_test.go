a83b7e021c5ceb05205e28b1a775ddd8b6e63c7f
fhasnur <fandi.hasnur10@gmail.com>
2024-11-14 21:26:14 +0700

    http request

--- golang-fiber/fiber_test.go
+++ golang-fiber/fiber_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
@@ -49,4 +50,23 @@ func TestCtx(t *testing.T) {
 	bytes, err = io.ReadAll(response.Body)
 	assert.Nil(t, err)
 	assert.Equal(t, "Hello Guest", string(bytes))
+}
+
+func TestHttpRequest(t *testing.T) {
+	app.Get("/request", func(ctx *fiber.Ctx) error {
+		first := ctx.Get("firstname")
+		last := ctx.Cookies("lastname")
+		return ctx.SendString("Hello " + first + " " + last)
+	})
+
+	request := httptest.NewRequest("GET", "/request", nil)
+	request.Header.Set("firstname", "Fandi")
+	request.AddCookie(&http.Cookie{Name: "lastname", Value: "Hasnur"})
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello Fandi Hasnur", string(bytes))
 }
