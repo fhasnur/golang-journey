74664ee2b8bd356102dfed100d011df853524963
fhasnur <fandi.hasnur10@gmail.com>
2024-11-18 21:00:52 +0700

    http response

--- golang-fiber/fiber_test.go
+++ golang-fiber/fiber_test.go
@@ -245,3 +245,22 @@ func TestBodyParserXML(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "Register Success Fandi", string(bytes))
 }
+
+func TestResponseJSON(t *testing.T) {
+	app.Get("/user", func(ctx *fiber.Ctx) error {
+		return ctx.JSON(fiber.Map{
+			"username": "fhasnur",
+			"name":     "Fandi Hasnur",
+		})
+	})
+
+	request := httptest.NewRequest("GET", "/user", nil)
+	request.Header.Set("Accept", "application/json")
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, `{"name":"Fandi Hasnur","username":"fhasnur"}`, string(bytes))
+}
