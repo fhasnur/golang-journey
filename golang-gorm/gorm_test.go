cf3297014e6e9b45592c2e1519622e4518518959
fhasnur <fandi.hasnur10@gmail.com>
2025-08-01 10:35:17 +0800

    logger

--- golang-gorm/gorm_test.go
+++ golang-gorm/gorm_test.go
@@ -7,11 +7,14 @@ import (
 	"github.com/stretchr/testify/assert"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func OpenConnection() *gorm.DB {
 	dialect := mysql.Open("root:123456789@tcp(localhost:3306)/golang_gorm?charset=utf8mb4&parseTime=True&loc=Local")
-	db, err := gorm.Open(dialect, &gorm.Config{})
+	db, err := gorm.Open(dialect, &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 	if err != nil {
 		panic(err)
 	}
