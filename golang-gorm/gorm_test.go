b1dae57f95b20edd7e185434ad9e29bda59ee195
fhasnur <fandi.hasnur10@gmail.com>
2025-08-04 21:58:49 +0800

    query non model

--- golang-gorm/gorm_test.go
+++ golang-gorm/gorm_test.go
@@ -1,6 +1,7 @@
 package golanggorm
 
 import (
+	"fmt"
 	"strconv"
 	"testing"
 
@@ -295,4 +296,18 @@ func TestOrderLimitOffset(t *testing.T) {
 	err := db.Order("id asc, first_name desc").Limit(5).Offset(5).Find(&users).Error
 	assert.Nil(t, err)
 	assert.Equal(t, 5, len(users))
+}
+
+type UserResponse struct {
+	ID        string
+	FirstName string
+	LastName  string
+}
+
+func TestQueryNonModel(t *testing.T) {
+	var users []UserResponse
+	err := db.Model(&User{}).Select("id", "first_name", "last_name").Find(&users).Error
+	assert.Nil(t, err)
+	assert.Equal(t, 14, len(users))
+	fmt.Println(users)
 }
