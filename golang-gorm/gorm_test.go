45b53507f1b77d49fb183aae001cd113822492e8
fhasnur <fandi.hasnur10@gmail.com>
2025-08-07 21:24:57 +0800

    lock

--- golang-gorm/gorm_test.go
+++ golang-gorm/gorm_test.go
@@ -451,3 +451,20 @@ func TestUnscoped(t *testing.T) {
 	err = db.Unscoped().Find(&todos).Error
 	assert.Nil(t, err)
 }
+
+func TestLock(t *testing.T) {
+	err := db.Transaction(func(tx *gorm.DB) error {
+		var user User
+		err := tx.Clauses(clause.Locking{Strength: "UPDATE"}).Take(&user, "id = ?", "11").Error
+		if err != nil {
+			return err
+		}
+
+		user.Name.FirstName = "Joko"
+		user.Name.LastName = "Morro"
+		err = tx.Save(&user).Error
+		return err
+	})
+
+	assert.Nil(t, err)
+}
