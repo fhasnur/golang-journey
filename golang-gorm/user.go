9b3e56930b071bcc4eb3b884095beaf75225baf5
fhasnur <fandi.hasnur10@gmail.com>
2025-08-07 21:51:33 +0800

    one to one

--- golang-gorm/user.go
+++ golang-gorm/user.go
@@ -9,6 +9,7 @@ type User struct {
 	CreatedAt   time.Time `gorm:"column:created_at;autoCreateTime;<-:create"`
 	UpdatedAt   time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
 	Information string    `gorm:"-"`
+	Wallet      Wallet    `gorm:"foreignKey:user_id;references:id"`
 }
 
 // Jika ingin mengubah nama tabel mapping-nya
